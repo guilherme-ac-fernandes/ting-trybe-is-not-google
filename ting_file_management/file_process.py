3fae3fb2ede02e2a701e374308aea042838feed2
Guilherme Fernandes <gfengquimica@gmail.com>
2023-02-03 15:55:51 -0300

    feat(req03): criação da função para adicionar dados do arquivo a fila informada

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+import sys
+
+
+def process(path_file, instance: Queue):
+    path_file_names = [instance.search(index)['nome_do_arquivo']
+                       for index in range(len(instance))]
+
+    if path_file not in path_file_names:
+        data = txt_importer(path_file)
+        dict_data = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': len(data),
+            'linhas_do_arquivo': data,
+        }
+
+        instance.enqueue(dict_data)
+        print(dict_data, file=sys.stdout)
 
 
 def remove(instance):
