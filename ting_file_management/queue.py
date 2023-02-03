8476ee2b4ab2042183b89e204c16164076c94263
Guilherme Fernandes <gfengquimica@gmail.com>
2023-02-03 15:06:24 -0300

    feat(req01): implementa classe fila

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,32 @@
 from ting_file_management.abstract_queue import AbstractQueue
+import sys
 
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui""" 
+        self._list = list() 
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._list)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._list.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self._list) == 0:
+            return None
+        return self._list.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self._list):
+            return self._list[index]
+        raise IndexError
+
+    def __str__(self):
+        str_items = ""
+        for i in range(len(self._list)):
+            value = self._list[i]
+            str_items += str(value)
+            if i + 1 < len(self._list):
+                str_items += ", "
+        return "Queue(" + str_items + ")"
