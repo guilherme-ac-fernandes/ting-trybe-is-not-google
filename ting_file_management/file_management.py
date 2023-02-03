4537255a89f309065b7f13b8f85fbf276b4d449a
Guilherme Fernandes <gfengquimica@gmail.com>
2023-02-03 15:31:04 -0300

    feat(req02): criação da função de importação de arquivo txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            return print('Formato inválido', file=sys.stderr)
+
+        with open(path_file, mode='r') as file:
+            file_created = [string.replace('\n', '') for string in file]
+
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+
+    else:
+        return file_created
