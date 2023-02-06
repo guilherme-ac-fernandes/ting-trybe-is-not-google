e81a272059df0ef19a9c7059b2cfa77930faa14f
Guilherme Fernandes <gfengquimica@gmail.com>
2023-02-06 15:36:04 -0300

    refactor(req06): adiciona mais casos de testes

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -22,18 +22,18 @@ mock_files_data = [
 
 def test_basic_priority_queueing():
     priority_queue = PriorityQueue()
-
     assert len(priority_queue) == 0
 
     for file_data in mock_files_data:
         priority_queue.enqueue(file_data)
 
     assert len(priority_queue) == len(mock_files_data)
-
     assert priority_queue.search(0) == mock_files_data[1]
     assert priority_queue.search(1) == mock_files_data[2]
     assert priority_queue.search(2) == mock_files_data[0]
     assert priority_queue.dequeue() == mock_files_data[1]
+    assert priority_queue.dequeue() == mock_files_data[2]
+    assert priority_queue.dequeue() == mock_files_data[0]
 
     with pytest.raises(IndexError):
         priority_queue.search(999)
