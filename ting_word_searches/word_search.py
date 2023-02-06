14dd816949789185d0f6cb501eaf426af11e7249
Guilherme Fernandes <gfengquimica@gmail.com>
2023-02-06 11:44:35 -0300

    feat(req08): criação da função para busca de palavra com geração de um relatório completo

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -20,5 +20,21 @@ def exists_word(word, instance: Queue):
     return word_search
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(word, instance: Queue):
+    word_search = []
+    for file_index in range(len(instance)):
+        file_data = instance.search(file_index)
+        lines_search = []
+        for position, line in enumerate(file_data['linhas_do_arquivo']):
+            if word.casefold() in line.casefold():
+                lines_search.append({
+                    "linha": position + 1,
+                    "conteudo": line,
+                })
+        if len(lines_search) > 0:
+            word_search.append({
+                "palavra": word,
+                "arquivo": file_data['nome_do_arquivo'],
+                "ocorrencias": lines_search
+            })
+    return word_search
